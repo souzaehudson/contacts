04c5144742f2e66cecd5ce5faa979769a37b23c7
Hudson Souza <souzaehudson@gmail.com>
2025-08-07 13:01:54 -0300

    Doze commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const menuButton = document.getElementById("menu");
 
+const linksListMobile = document.getElementById("mobileMenu");
+
+const bodyHtml = document.getElementById("body");
+const mainHtml = document.getElementById("main");
+
+function showLinksMobile () {
+  linksListMobile.style.display = "flex";
+  menuButton.style.display = "none";
+};
+
+function closeLinksMobile () {
+  linksListMobile.style.display = "none";
+  menuButton.style.display = "flex";
+};
+
+menuButton.addEventListener("click", showLinksMobile);
+main.addEventListener("click", closeLinksMobile);
